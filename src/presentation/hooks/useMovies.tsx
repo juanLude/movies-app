57cf0019a728aef2b1a6986844af80c14a105e80
Juan Ludevid <jmludevid@hotmail.com>
2024-03-26 21:49:37 +1100

    add Promise.all

--- src/presentation/hooks/useMovies.tsx
+++ src/presentation/hooks/useMovies.tsx
@@ -5,18 +5,35 @@ import {useEffect, useState} from 'react';
 
 export const useMovies = () => {
   const [isLoading, setisLoading] = useState(true);
-  const [nowPlaying, setnowPlaying] = useState<Movie>([]);
+  const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+  const [popular, setPopular] = useState<Movie[]>([]);
+  const [topRated, setTopRated] = useState<Movie[]>([]);
+  const [upcoming, setUpcoming] = useState<Movie[]>([]);
 
   useEffect(() => {
     initialLoad();
   }, []);
 
   const initialLoad = async () => {
-    const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(
-      movieDBFetcher,
-    );
-    console.log(nowPlayingMovies[0]);
+    const nowPlayingPromise = UseCases.moviesNowPlayingUseCase(movieDBFetcher);
+    const popularPromise = UseCases.moviesPopularUseCase(movieDBFetcher);
+    const topRatedPromise = UseCases.moviesTopRatedUseCase(movieDBFetcher);
+    const upcomingPromise = UseCases.moviesUpcomingUseCase(movieDBFetcher);
+
+    const [nowPlayingMovies, popularMovies, topRatedMovies, upcomingMovies] =
+      await Promise.all([
+        nowPlayingPromise,
+        popularPromise,
+        topRatedPromise,
+        upcomingPromise,
+      ]);
+
+    setNowPlaying(nowPlayingMovies);
+    setPopular(popularMovies);
+    setTopRated(topRatedMovies);
+    setUpcoming(upcomingMovies);
+    setisLoading(false);
   };
 
-  return {isLoading, nowPlaying};
+  return {isLoading, nowPlaying, popular, topRated, upcoming};
 };
