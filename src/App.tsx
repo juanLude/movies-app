5cc8410f6e128b56542eb7b4e6a9b5c913723112
Juan Ludevid <jmludevid@hotmail.com>
2024-03-27 16:53:23 +1100

    add poster carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text, View} from 'react-native';
+
+import {Navigation} from './presentation/navigation/Navigation';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>MoviesApp</Text>
-      </View>
+      <Navigation />
     </NavigationContainer>
   );
 };
---
+++ src/presentation/components/movies/MoviePoster.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Image, Pressable, StyleSheet, View} from 'react-native';
+import {Movie} from '../../../core/entities/movie.entity';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParams} from '../../navigation/Navigation';
+
+interface Props {
+  movie: Movie;
+  height?: number;
+  width?: number;
+}
+
+export const MoviePoster = ({movie, height = 420, width = 300}: Props) => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+  return (
+    <Pressable
+      onPress={() => navigation.navigate('Details', {movieId: movie.id})}>
+      <View style={{...styles.imageContainer, width: width, height: height}}>
+        <Image style={styles.image} source={{uri: movie.poster}} />
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    borderRadius: 18,
+  },
+  imageContainer: {
+    flex: 1,
+    borderRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+
+    elevation: 9,
+  },
+});
